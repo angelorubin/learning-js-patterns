f50ce98789bec65a281764bc4e4611c21396fbca
Angelo Rubin <angelorubin@gmail.com>
2021-07-10 15:04:49 -0300

    Removendo arquivos com dados sensíveis

--- padroes-criacionais/factory/src/utils/DbFactory.js
+++ padroes-criacionais/factory/src/utils/DbFactory.js
@@ -1,30 +1,6 @@
 const mysql = require("mysql2");
 const { Client } = require("pg");
 
-/*
-const dbConnect = async () => {
-	try {
-		const connection = await mysql.createConnection({
-			host: "127.0.0.1",
-			port: "3306",
-			user: "root",
-			password: "root",
-			database: "test",
-		});
-
-		// console.log("mysql connection success");
-
-		connection.query("SELECT * FROM users", function (err, results, fields) {
-			console.log(results);
-		});
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-dbConnect();
-*/
-
 class MySql {
 	constructor({ type, host, port, database, user, password }) {
 		this.type = type;
