f86140b434e85d7ea5e96ec60aba37586ce736ad
angelorubin <angelorubin@gmail.com>
2021-07-11 16:16:49 -0300

    Alterando variáveis de ambiente

--- creational-patterns/factory/src/address.js
+++ creational-patterns/factory/src/address.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
 const DbFactory = require("./utils/DbFactory");
+require("dotenv").config();
 
 const dbFactory = new DbFactory();
 
 const pgConn = dbFactory.createConnection({
-	type: "postgres",
-	host: process.env.PG_HOST,
-	port: "5432",
-	database: "postgres-db",
-	user: "postgres",
-	password: "root",
+  type: "postgres",
+  host: process.env.P_HOST,
+  port: "5432",
+  database: process.env.P_DATABASE,
+  user: process.env.P_USER,
+  password: process.env.P_PASSWORD,
 });
 
 const pgDb = pgConn.connection();
 
 pgDb.query("SELECT * FROM users", function (err, res) {
-	if (err) {
-		console.log(`ERROR: ${err}`);
-	}
-	console.log(`Dados vindos do Postgres`, res.rows[0]);
+  if (err) {
+    console.log(`ERROR: ${err}`);
+  }
+  console.log(`Dados vindos do Postgres`, res.rows[0]);
 });
--- creational-patterns/factory/src/users.js
+++ creational-patterns/factory/src/users.js
@@ -4,19 +4,19 @@ const DbFactory = require("./utils/DbFactory");
 const dbFactory = new DbFactory();
 
 const mysqlConn = dbFactory.createConnection({
-	type: "mysql",
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	database: process.env.MYSQL_DATABASE,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_ROOT_PASSWORD,
+  type: "mysql",
+  host: process.env.M_HOST,
+  port: process.env.M_PORT,
+  database: process.env.M_DATABASE,
+  user: process.env.M_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
 });
 
 const mysqlDb = mysqlConn.connection();
 
 mysqlDb.query("SELECT * FROM users", function (err, res) {
-	if (err) {
-		console.log(`ERROR: ${err}`);
-	}
-	console.log(`Dados vindos do MySQL`, res);
+  if (err) {
+    console.log(`ERROR: ${err}`);
+  }
+  console.log(`Dados vindos do MySQL`, res);
 });
